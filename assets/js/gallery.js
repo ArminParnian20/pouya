bb97c4a5915ee6f7a2adbcc95edaaf91cbb3ea86
Armin <arminparnian182@gmail.com>
2021-11-09 12:42:09 +0330

    10

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -5,7 +5,36 @@ let closeIcone=previewBox.querySelector('.icon')
 let totalImg=document.querySelector('.total-img');
 let currentImg=document.querySelector('.current-img')
 let shadow=document.querySelector('.shadow');
+let loading=document.querySelector('.load-box');
+let body=document.querySelector('.website');
 
+window.addEventListener('load',()=>{
+  setTimeout(()=>{
+    body.style.display='block'; 
+    loading.style.display='none';
+  },1000)
+})
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+setInterval(setDate, 1000);
+setDate();
 totalImg.textContent=gallery.length;
 for(let i=0;i<gallery.length;i++){
        let newIndex=i;
@@ -57,6 +86,7 @@ for(let i=0;i<gallery.length;i++){
            nextBtn.style.display="block"
            previewBox.classList.remove('show');
            shadow.style.display="none";
+           newIndex=0;
 
           }) 
         });
